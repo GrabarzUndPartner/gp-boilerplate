8dbdf029740495d4e3735e5ea4e31d78d28911fe
Thorn Walli <thorn.walli@grabarzundpartner.de>
2018-02-27 12:06:17 +0100

    update

--- env/config/hapi/config.js
+++ env/config/hapi/config.js
@@ -1,16 +1,32 @@
 "use strict";
 
+var fs = require('fs');
+var path = require('path');
+var osHomedir = require('os-homedir');
+
+var hapi = {
+    host: 'localhost',
+    port: 8050,
+    secret: 'UNSAFE: CHANGE ME',
+    routes: require('./routes')
+};
+
+
+
+var tlsPath = path.resolve(osHomedir() + '/certificates/tls');
+console.log(tlsPath);
+if (fs.existsSync(tlsPath)) {
+    hapi.tls = {
+        key: fs.readFileSync(path.resolve(tlsPath + '/privateKey.key')),
+        cert: fs.readFileSync(path.resolve(tlsPath + '/certificate.crt'))
+    };
+}
+
 module.exports = {
     dev: (process.env.NODE_ENV === 'development'),
     dest: (process.env.NODE_ENV === 'development') ? 'dev' : process.env.NODE_ENV,
     root: './' + ((process.env.NODE_ENV === 'development') ? '' : process.env.NODE_ENV),
 
-    hapi: {
-        host: 'localhost',
-        port: 8050,
-        secret: 'UNSAFE: CHANGE ME',
-        routes: require('./routes')
-    },
-
+    hapi: hapi,
     servers: []
 };
